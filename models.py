513d589b1ef6fb0e9c0b234696cf5a888c3cb88f
Nicole McDuffie <nicole.mcduffie@junomecial.com>
2020-08-14 22:28:17 -0400

    add revoked token model

--- models.py
+++ models.py
@@ -24,3 +24,16 @@ class User(db.Model):
     def verify_hash(password, hash):
         return pbkdf2_sha256.verify(password, hash)
 
+class RevokedToken(db.Model):
+    __tablename__ = 'revoked_tokens'
+    id = db.Column(db.Integer, primary_key = True)
+    jti = db.Column(db.String(120))
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def is_jti_blacklisted(cls, jti):
+        query = cls.query.filter_by(jti=jti).first()
+        return bool(query)
