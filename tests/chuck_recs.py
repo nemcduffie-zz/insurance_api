ba548af720312fc2fccd92b970130fb7aa9e19c2
Nicole McDuffie <nicole.mcduffie@junomecial.com>
2020-08-17 23:07:59 -0400

    add questionaire testing

---
+++ tests/chuck_recs.py
@@ -0,0 +1,39 @@
+CHUCK_RECS = {
+    'needed': [
+        {
+            'type':'Personal Liability',
+            'conpany':'Coya',
+            'price_monthly':'€4.99'
+        }, {
+            'type':'Job',
+            'conpany':'Community Life',
+            'price_monthly':'€30.00'
+        }, {
+            'type':'Life',
+            'conpany':'Community Life',
+            'price_monthly':'€20.00'
+        }, {
+            'type':'Health (Public)',
+            'conpany':'Techniker Krankenkasse',
+            'price_monthly':'€281.25'
+        }
+    ],
+    'optional': [
+        {
+            'type':'Car',
+            'conpany':'Fri:day',
+            'price_monthly':'€75.00'
+        }, {
+            'type':'Household Content',
+            'conpany':'One Insurance',
+            'price_monthly':'€7.50'
+        }
+    ],
+    'not_needed': [
+        {
+            'type':'Health (Private)',
+            'conpany':'Ottonova',
+            'price_monthly':'€237.80'
+        }
+    ]
+}
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,4 +1,5 @@
 from flask import json
+from tests.chuck_recs import CHUCK_RECS
 
 
 def test_token_access(app, client) -> None:
@@ -54,3 +55,43 @@ def test_token_access(app, client) -> None:
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 401
+
+
+def test_questionaire(app, client) -> None:
+    response = client.post(
+        '/login',
+        data=json.dumps({'username': 'test',
+                         'password': 'testpw'}),
+        content_type='application/json')
+
+    assert response.status_code == 200
+    data = json.loads(response.get_data(as_text=True))
+    token = data.get('access_token')
+    assert token
+    headers = {'Authorization': 'Bearer {}'.format(token)}
+
+    user_info = {
+        'name': 'Chuck',
+        'address': '15774 Longwood Dr. Los Gatos CA 95032',
+        'children': True,
+        'num_children': 1,
+        'occupation': 'Python Developer',
+        'occupation_type': 'Employed',
+        'email': 'chuck@chuck.com'
+    }    
+    response = client.post(
+        '/questionaire',
+        headers=headers,
+        data=json.dumps(user_info),
+        content_type='application/json')
+
+    assert response.status_code == 200
+    data = json.loads(response.get_data(as_text=True))
+    assert data.get('recommendations') == CHUCK_RECS
+    response = client.post(
+        '/logout',
+        headers=headers,
+        content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
