6156d9355f6b9e51fcadab134fa75c8440fce3a9
Nicole McDuffie <nicole.mcduffie@junomecial.com>
2020-08-18 14:09:54 -0400

    fix missing status codes and tidying

--- insurance_api/resources.py
+++ insurance_api/resources.py
@@ -1,15 +1,18 @@
 from typing import Dict, Tuple
 from flask_restful import Resource, reqparse
 from flask_restplus import inputs
+from sqlalchemy import exc
 from insurance_api.models import User, RevokedToken
 from insurance_api.insurance_recs import insurance_recs
 from flask_jwt_extended import (create_access_token, create_refresh_token,
                                 jwt_required, get_jwt_identity, get_raw_jwt)
 
 
+HELP = 'This field cannot be blank'
+
 user_parser = reqparse.RequestParser()
-user_parser.add_argument('username', help ='This field cannot be blank', required=True)
-user_parser.add_argument('password', help ='This field cannot be blank', required=True)
+user_parser.add_argument('username', help=HELP, required=True)
+user_parser.add_argument('password', help=HELP, required=True)
 
 
 class Registration(Resource):
@@ -17,7 +20,9 @@ class Registration(Resource):
         data = user_parser.parse_args()
         
         if User.find_user(data['username']):
-            return {'message': 'User {} already exists'.format(data['username'])} #TODO: status code
+            return {
+                'message': 'User {} already exists'.format(data['username'])
+            }, 422
         
         new_user = User(
             username = data['username'],
@@ -29,11 +34,12 @@ class Registration(Resource):
             access_token = create_access_token(identity=data['username'])
             refresh_token = create_refresh_token(identity=data['username'])
             return {
-                'message': 'User {} was successfully created'.format(data['username']),
+                'message': 'User {} was successfully created'.format(
+                    data['username']),
                 'access_token': access_token,
                 'refresh_token': refresh_token
-                }, 200
-        except:
+            }, 200
+        except exc.SQLAlchemyError:
             return {'message': 'User wasn\'t able to be added to db'}, 500
 
 
@@ -49,9 +55,9 @@ class Login(Resource):
                 'message': 'Logged in as {}'.format(curr_user.username),
                 'access_token': access_token,
                 'refresh_token': refresh_token
-                }, 200
+            }, 200
         else:
-            return {'message': 'Wrong credentials'}, 404
+            return {'message': 'Wrong credentials'}, 422
 
 
 class Logout(Resource):
@@ -67,13 +73,14 @@ class Logout(Resource):
 
 
 questionaire_parser = reqparse.RequestParser()
-questionaire_parser.add_argument('name', help ='This field cannot be blank', required=True)
-questionaire_parser.add_argument('address', help ='This field cannot be blank', required=True)
-questionaire_parser.add_argument('children', type=inputs.boolean, help ='This field cannot be blank', required=True)
+questionaire_parser.add_argument('name', help=HELP, required=True)
+questionaire_parser.add_argument('address', help=HELP, required=True)
+questionaire_parser.add_argument('children', type=inputs.boolean,
+                                 help=HELP, required=True)
 questionaire_parser.add_argument('num_children', type=int, required=False)  #TODO: validate if above is false
-questionaire_parser.add_argument('occupation', help ='This field cannot be blank', required=True)
-questionaire_parser.add_argument('occupation_type', help ='This field cannot be blank', required=True)  #TODO: validate between three choices? Employed, Student, Self-employed
-questionaire_parser.add_argument('email', help ='This field cannot be blank', required=True)
+questionaire_parser.add_argument('occupation', help=HELP, required=True)
+questionaire_parser.add_argument('occupation_type', help=HELP, required=True)  #TODO: validate between three choices? Employed, Student, Self-employed
+questionaire_parser.add_argument('email', help=HELP, required=True)
 
 
 class Questionaire(Resource):
@@ -83,11 +90,12 @@ class Questionaire(Resource):
         username = get_jwt_identity()
         user = User.find_user(username)
         if not user:
-            return {'message': 'Wrong credentials'}  # TODO: status code
+            return {'message': 'Wrong credentials'}, 422
 
         for field in data.keys():
             setattr(user, field, data[field])
         user.save()
+
         return {
             'message': 'User {} was successfully updated'.format(username),
             'recommendations': insurance_recs(user)
@@ -98,14 +106,13 @@ class Questionaire(Resource):
         username = get_jwt_identity()
         user = User.find_user(username)
         if not user:
-            return {'message': 'Wrong credentials'}  # TODO: status code
+            return {'message': 'Wrong credentials'}, 422
         if not user.name:
-            return {'message': 'Please fill in your user infromation'}  # TODO: status code
+            return {'message': 'Please fill in your user infromation'}, 422
         return {
-            'message': 'User {}\'s recommendations successfully collected'.format(username),
+            'message': 'User\'s recommendations successfully collected',
             'recommendations': insurance_recs(user)
         }, 200
-
 
 
 class Secret(Resource):
