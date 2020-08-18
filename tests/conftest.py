5a0c3c35c0f14d9d6d0e78466bc1b8e38facadc4
Nicole McDuffie <nicole.mcduffie@junomecial.com>
2020-08-17 22:42:28 -0400

    reformat testing fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from run import create_app
+from insurance_api.models import db
+from insurance_api.config import config_settings
+
+
+@pytest.fixture(scope='session')
+def app():
+    app = create_app('testing')
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture(scope='session')
+def client(app):
+    client = app.test_client()
+    return client
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,29 +1,7 @@
-
 from flask import json
-import pytest
-
-from run import create_app
-from insurance_api.models import db
-from insurance_api.config import config_settings
 
 
-@pytest.fixture(scope='session')
-def app():
-    app = create_app('testing')
-    with app.app_context():
-        db.create_all()
-        yield app
-        db.session.remove()
-        db.drop_all()
-
-
-@pytest.fixture(scope='session')
-def client(app):
-    client = app.test_client()
-    return client
-
-
-def test_resources(app, client) -> None:
+def test_token_access(app, client) -> None:
     response = client.get(
         '/secret',
         content_type='application/json')
