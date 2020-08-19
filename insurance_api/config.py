eb5bd9eb137f2b2a375262ebe69bb3ae466d0cb5
Nicole McDuffie <nicole.mcduffie@junomecial.com>
2020-08-18 21:51:29 -0400

    fix pytest env variable issue

--- insurance_api/config.py
+++ insurance_api/config.py
@@ -18,10 +18,10 @@ class TestConfig():
     """ Configuration for testing the app.
     """
     DEBUG = False
-    # SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_TEST_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'testing-secret-key'
-    JWT_SECRET_KEY = 'testing-jwt-secret-key'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     PROPAGATE_EXCEPTIONS = True
